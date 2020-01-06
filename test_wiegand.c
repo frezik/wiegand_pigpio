97fd06fdb740a6b37fc4a6238b7ebce734107acc
Timm Murray <tmurray@wumpus-cave.net>
2020-01-06 16:16:01 -0600

    Parse command line options for pin assignments

--- test_wiegand.c
+++ test_wiegand.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <pigpio.h>
 
@@ -28,10 +29,19 @@ void callback(int bits, uint32_t value)
 int main(int argc, char *argv[])
 {
     Pi_Wieg_t * w;
+    int gpio0 = 17;
+    int gpio1 = 18;
+
+    if( 3 > argc ) {
+        printf( "Usage: %s <pin0> <pin1>\n", argv[0] );
+        return 0;
+    }
+    gpio0 = atoi( argv[1] );
+    gpio1 = atoi( argv[2] );
 
     if (gpioInitialise() < 0) return 1;
 
-    w = Pi_Wieg(17, 18, callback, 5);
+    w = Pi_Wieg( gpio0, gpio1, callback, 5 );
     sleep(300);
 
     Pi_Wieg_cancel(w);
